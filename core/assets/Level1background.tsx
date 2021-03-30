c2f355bc6cdc105d3a9982670c801cfcc4833aec
Marsild <marsild.spahiu@studio.unibo.it>
2021-03-30 18:35:11 +0200

    added assets and background in the levels

---
+++ core/assets/Level1background.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="Level1background" tilewidth="16" tileheight="16" tilecount="858" columns="39">
+ <image source="Level1background.jpg" width="634" height="360"/>
+</tileset>
---
+++ core/assets/NinjaFrogAsset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="NinjaFrogAsset" tilewidth="16" tileheight="16" tilecount="200" columns="10">
+ <image source="NinjaFrogAsset.png" width="160" height="320"/>
+</tileset>
