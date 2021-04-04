fe56d9e3e2219970e6ccb509eb00c64b478824fe
Marsild <marsild.spahiu@studio.unibo.it>
2021-04-04 20:47:43 +0200

    Add javaDoc in WorldCreator

--- core/src/it/unibo/ninjafrog/world/WorldCreator.java
+++ core/src/it/unibo/ninjafrog/world/WorldCreator.java
@@ -1,5 +1,11 @@
 package it.unibo.ninjafrog.world;
 
+/**
+ * WorldCreator interface definition.
+ */
 public interface WorldCreator {
+    /**
+     * Method to be called in order to create the solid world from the tiled Map.
+     */
     void createWorld();
 }
