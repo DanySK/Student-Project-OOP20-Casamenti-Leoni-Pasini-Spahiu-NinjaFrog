9a4c9e6d3b058e74e119b7c00b3954e288e1a14f
Marsild <marsild.spahiu@studio.unibo.it>
2021-04-04 15:11:53 +0200

    Changed ninjafroggame super-class to Game class

--- core/src/it/unibo/ninjafrog/game/NinjaFrogGame.java
+++ core/src/it/unibo/ninjafrog/game/NinjaFrogGame.java
@@ -1,11 +1,11 @@
 package it.unibo.ninjafrog.game;
 
-import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public final class NinjaFrogGame extends ApplicationAdapter {
+public final class NinjaFrogGame extends Game {
     private SpriteBatch batch;
 
     @Override
