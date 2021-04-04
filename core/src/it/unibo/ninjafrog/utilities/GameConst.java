526c03f735da51f79eabadbc73123e3f9261fec0
Marsild <marsild.spahiu@studio.unibo.it>
2021-04-04 15:31:31 +0200

    Added constants to GameConst

--- core/src/it/unibo/ninjafrog/utilities/GameConst.java
+++ core/src/it/unibo/ninjafrog/utilities/GameConst.java
@@ -2,4 +2,102 @@ package it.unibo.ninjafrog.utilities;
 
 public final class GameConst {
 
+    /**
+     * 
+     */
+    public static final int WIDTH = 400;
+    /**
+     * 
+     */
+    public static final int HEIGHT = 208;
+
+    /**
+     * 
+     */
+    public static final float PPM = 100;
+
+    /**
+     * 
+     */
+    public static final short GROUND = 1;
+    /**
+     * 
+     */
+    public static final short NINJA = 2;
+    /**
+     * 
+     */
+    public static final short BRICK = 4;
+    /**
+     * 
+     */
+    public static final short FRUITBOX = 8;
+    /**
+     * 
+     */
+    public static final short DESTROYED = 16;
+    /**
+     * 
+     */
+    public static final short RINO = 32;
+    /**
+     * 
+     */
+    public static final short RINO_HEAD = 64;
+    /**
+     * 
+     */
+    public static final short TURTLE = 128;
+    /**
+     * 
+     */
+    public static final short TURTLE_HEAD = 256;
+    /**
+     * 
+     */
+    public static final short GROUND_OBJECT = 512;
+    /**
+     * 
+     */
+    public static final short NINJA_HEAD = 1024;
+    /**
+     * 
+     */
+    public static final short FRUIT = 2048;
+    /**
+     * 
+     */
+    public static final short FINISH = 4096;
+
+    /**
+     * 
+     */
+    public static final int GROUND_LAYER = 2;
+    /**
+     * 
+     */
+    public static final int FRUITBOX_LAYER = 3;
+    /**
+     * 
+     */
+    public static final int BRICK_LAYER = 4;
+    /**
+     * 
+     */
+    public static final int GROUND_OBJECT_LAYER = 5;
+    /**
+     * 
+     */
+    public static final int FINISH_LAYER = 6;
+    /**
+     * 
+     */
+    public static final int RINO_LAYER = 7;
+    /**
+     * 
+     */
+    public static final int TURTLE_LAYER = 8;
+
+    private GameConst() {
+    }
 }
