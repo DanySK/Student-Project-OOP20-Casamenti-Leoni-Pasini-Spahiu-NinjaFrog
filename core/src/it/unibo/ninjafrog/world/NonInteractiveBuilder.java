2d60c721a6a41e86f17b77a45aaede45a5bb0ce9
Marsild <marsild.spahiu@studio.unibo.it>
2021-04-06 15:04:07 +0200

    Add methods to builder interface

--- core/src/it/unibo/ninjafrog/world/NonInteractiveBuilder.java
+++ core/src/it/unibo/ninjafrog/world/NonInteractiveBuilder.java
@@ -1,5 +1,10 @@
 package it.unibo.ninjafrog.world;
 
+import com.badlogic.gdx.maps.MapObject;
+
 public interface NonInteractiveBuilder {
 
+    NonInteractiveBuilder selectObject(MapObject object);
+    NonInteractiveBuilder chooseCategoryBit(short bit);
+    NonInteractiveObject build();
 }
