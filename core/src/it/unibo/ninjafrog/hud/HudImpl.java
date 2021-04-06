0db910638d8fed2791b6a68bbba140d4faa7677e
LoryBug <lorenzo.leoni5@studio.unibo.it>
2021-04-06 10:09:28 +0200

    fixed Hud and added HudImpl

---
+++ core/src/it/unibo/ninjafrog/hud/HudImpl.java
@@ -0,0 +1,38 @@
+package it.unibo.ninjafrog.hud;
+
+import com.badlogic.gdx.utils.Disposable;
+
+public class HudImpl implements Hud, Disposable{
+
+    private Integer score;
+    private Integer life;
+
+    /*public Hud() {
+
+    }*/
+
+    @Override
+    public void dispose() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void addScore(final int i) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void addLife() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void removeLife() {
+        // TODO Auto-generated method stub
+
+    }
+
+}
