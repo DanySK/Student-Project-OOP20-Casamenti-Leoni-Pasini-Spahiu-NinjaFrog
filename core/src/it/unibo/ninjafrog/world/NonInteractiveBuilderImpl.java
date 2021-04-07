fd070a8fa5163f8e2a6c9f8ad2e01e80b6745edf
Marsild <marsild.spahiu@studio.unibo.it>
2021-04-07 15:33:21 +0200

    Create BuilderImpl

---
+++ core/src/it/unibo/ninjafrog/world/NonInteractiveBuilderImpl.java
@@ -0,0 +1,25 @@
+package it.unibo.ninjafrog.world;
+
+import com.badlogic.gdx.maps.MapObject;
+
+public final class NonInteractiveBuilderImpl implements NonInteractiveBuilder {
+
+    @Override
+    public NonInteractiveBuilder selectObject(final MapObject object) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public NonInteractiveBuilder chooseCategoryBit(final short bit) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public NonInteractiveObject build() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
