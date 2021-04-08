d38fd6e620df90ca7ffcb115d4fc01ca8aa1c96d
Gianmaria C <gianmaria.casamenti@studio.unibo.it>
2021-04-08 16:54:05 +0200

    add FruitBuilderImpl and FruitPowerUpImpl

---
+++ core/src/it/unibo/ninjafrog/fruits/FruitPowerUpImpl.java
@@ -0,0 +1,36 @@
+package it.unibo.ninjafrog.fruits;
+
+import com.badlogic.gdx.graphics.g2d.Batch;
+
+import it.unibo.ninjafrog.screens.PlayScreen;
+
+public class FruitPowerUpImpl implements FruitPowerUp {
+    
+    public FruitPowerUpImpl(final PlayScreen screen, final float x, final float y, final FruitType type) {
+        
+    }
+    @Override
+    public void collide() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void draw(final Batch batch) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void update(final float dt) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void reverseVelocity() {
+        // TODO Auto-generated method stub
+
+    }
+
+}
