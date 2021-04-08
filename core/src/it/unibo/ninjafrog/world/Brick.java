627281e31e6984153726d4a406ce9e7862866d44
Marsild <marsild.spahiu@studio.unibo.it>
2021-04-08 10:17:12 +0200

    Create InteractiveObject class and extensions

---
+++ core/src/it/unibo/ninjafrog/world/Brick.java
@@ -0,0 +1,14 @@
+package it.unibo.ninjafrog.world;
+
+/**
+ * Brick class definition. Implementation of Collidable interface.
+ */
+public class Brick extends InteractiveObject implements Collidable {
+
+    @Override
+    public void collide() {
+        // TODO Auto-generated method stub
+
+    }
+
+}
---
+++ core/src/it/unibo/ninjafrog/world/InteractiveObject.java
@@ -0,0 +1,8 @@
+package it.unibo.ninjafrog.world;
+
+/**
+ * InteractiveObject class definition. To be extended.
+ */
+public class InteractiveObject {
+
+}
