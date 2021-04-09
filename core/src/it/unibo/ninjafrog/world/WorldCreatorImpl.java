e40f06a7d79adff316173e4d41c9ed166f939c69
Marsild <marsild.spahiu@studio.unibo.it>
2021-04-09 15:02:14 +0200

    implement worldcreatorimpl

--- core/src/it/unibo/ninjafrog/world/WorldCreatorImpl.java
+++ core/src/it/unibo/ninjafrog/world/WorldCreatorImpl.java
@@ -1,11 +1,53 @@
 package it.unibo.ninjafrog.world;
 
-public class WorldCreatorImpl implements WorldCreator {
+import com.badlogic.gdx.maps.MapObject;
+import com.badlogic.gdx.maps.objects.RectangleMapObject;
+import com.badlogic.gdx.maps.tiled.TiledMap;
 
+import it.unibo.ninjafrog.screens.PlayScreen;
+import it.unibo.ninjafrog.utilities.GameConst;
+
+public final class WorldCreatorImpl implements WorldCreator {
+
+    private final PlayScreen screen;
+    private final NonInteractiveBuilder builder;
+    public WorldCreatorImpl(final PlayScreen screen) {
+        this.screen = screen;
+        this.builder = new NonInteractiveBuilderImpl(screen);
+    }
     @Override
     public void createWorld() {
-        // TODO Auto-generated method stub
-
+        final TiledMap map = this.screen.getMap();
+        for (int i = GameConst.GROUND_LAYER; i <= GameConst.FINISH_TROPHY_LAYER; i++) {
+            for (final MapObject object : map.getLayers().get(i).getObjects().getByType(RectangleMapObject.class)) {
+                switch (i) {
+                case GameConst.GROUND_LAYER:
+                    this.builder.selectObject(object)
+                    .chooseCategoryBit(GameConst.GROUND)
+                    .build();
+                    break;
+                case GameConst.GROUND_OBJECT_LAYER:
+                    this.builder.selectObject(object)
+                    .chooseCategoryBit(GameConst.GROUND_OBJECT)
+                    .build();
+                    break;
+                case GameConst.FINISH_TROPHY_LAYER:
+                    this.builder.selectObject(object)
+                    .chooseCategoryBit(GameConst.FINISH)
+                    .build();
+                    break;
+                case GameConst.FRUITBOX_LAYER:
+                    new FruitBox(this.screen, object);
+                    break;
+                case GameConst.BRICK_LAYER:
+                    new Brick(this.screen, object);
+                    break;
+                default:
+                    //unused;
+                    break;
+                }
+            }
+        }
     }
 
 }
