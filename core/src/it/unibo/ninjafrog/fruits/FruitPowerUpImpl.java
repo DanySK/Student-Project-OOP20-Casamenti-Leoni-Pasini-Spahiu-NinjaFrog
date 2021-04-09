5d0c33e1c0f93e97cca7bf82cc89513f9ca02572
Gianmaria C <gianmaria.casamenti@studio.unibo.it>
2021-04-09 14:55:17 +0200

    modify FruitPowerUpImpl

--- core/src/it/unibo/ninjafrog/fruits/FruitPowerUpImpl.java
+++ core/src/it/unibo/ninjafrog/fruits/FruitPowerUpImpl.java
@@ -45,90 +45,125 @@ public class FruitPowerUpImpl extends Sprite implements FruitPowerUp {
        setBounds(getX(), getY(), BOUNDS_WIDTH, BOUNDS_HEIGHT);
        defineItem(this.type);
     }
-    private void defineItem(final FruitType type) {
-       switch (type) {
-       case MELON:
-           defineMelon();
-           break;
-       case ORANGE:
-           defineOrange();
-           break;
-       case CHERRY:
-           defineCherry();
-           break;
-       default:
-            break;
-       }
-    }
-    private void defineCherry() {
-        setRegion(screen.getAtlas().findRegion("NinjaAndEnemies"), 455, 9, 19, 16);
-        BodyDef cherryBody = new BodyDef();
-        cherryBody.position.set(getX(),getY());
-        cherryBody.type = BodyDef.BodyType.DynamicBody;
-        body = world.createBody(cherryBody);
-        FixtureDef cherryFixture = new FixtureDef();
-        CircleShape cherryShape = new CircleShape();
-        cherryShape.setRadius(FRUIT_RADIUS);
-        cherryFixture.shape = cherryShape;
-        maskBits(cherryFixture);
-        body.createFixture(cherryFixture).setUserData(this);
-    }
-    private void defineOrange() {
-        setRegion(screen.getAtlas().findRegion(REGION), 520, 9, 19, 16);
-        BodyDef orangeBody = new BodyDef();
-        orangeBody.position.set(getX(),getY());
-        orangeBody.type = BodyDef.BodyType.StaticBody;
-        body = world.createBody(orangeBody);
-        FixtureDef orangeFixture = new FixtureDef();
-        CircleShape orangeShape = new CircleShape();
-        orangeShape.setRadius(FRUIT_RADIUS);
-        orangeFixture.shape = orangeShape;
-        maskBits(orangeFixture);
-        body.createFixture(orangeFixture).setUserData(this);
-    }
-    private void defineMelon() {
-        setRegion(screen.getAtlas().findRegion(REGION), 486, 9, 19, 16);
-        BodyDef melonBody = new BodyDef();
-        melonBody.position.set(getX(), getY());
-        melonBody.type = BodyDef.BodyType.DynamicBody;
-        body = world.createBody(melonBody);
-        FixtureDef melonFixture = new FixtureDef();
-        CircleShape melonShape = new CircleShape();
-        melonShape.setRadius(FRUIT_RADIUS);
-        melonFixture.shape = melonShape;
-        maskBits(melonFixture);
-        body.createFixture(melonFixture).setUserData(this);
-    }
-    private void maskBits(final FixtureDef fruitFixture) {
-        fruitFixture.filter.categoryBits = NinjaFrog.ITEM_BIT;
-        fruitFixture.filter.maskBits = NinjaFrog.NINJA_BIT |
-                        NinjaFrog.GROUND_BIT |
-                        NinjaFrog.GROUNDOBJECT_BIT |
-                        NinjaFrog.BRICK_BIT |
-                        NinjaFrog.COIN_BIT ;
-    }
+    /**
+     * Implements collide method that is part of Collidable interface.
+     */
     @Override
     public void collide() {
-        // TODO Auto-generated method stub
+        switch (type) {
+        case MELON:
+            destroy();
+            //ninja.setdoublejump();
+            //Hud.addScore(GameConst.MELON_SCORE)
+            break;
+        case ORANGE:
+            destroy();
+            defineOrange();
+            //Hud.addScore(GameConst.ORANGE_SCORE);
+            break;
+        case CHERRY:
+            destroy();
+            defineCherry();
+            //ninja.addlife();
+            //Hud.addScore(GameConst.CHERRY_SCORE);
+            break;
+        default:
+             break;
+        }
 
     }
-
+    /**
+     * Implements draw method that is part of FruitPowerUp interface.
+     */
     @Override
     public void draw(final Batch batch) {
-        // TODO Auto-generated method stub
-
+        if (!destroyed) {
+            super.draw(batch);
+        }
     }
-
+    /**
+     * Implements update method that is part of FruitPowerUp interface.
+     */
     @Override
     public void update(final float dt) {
-        // TODO Auto-generated method stub
-
+        if (toDestroy && !destroyed) {
+            world.destroyBody(body);
+            destroyed = true;
+        }
+        setPosition(body.getPosition().x - getWidth() / 2, body.getPosition().y - getHeight() / 2);
+        body.setLinearVelocity(velocity);
     }
-
+    /**
+     * Implements reverseVelocity method that is part of FruitPowerUp interface.
+     */
     @Override
     public void reverseVelocity() {
-        // TODO Auto-generated method stub
-
+        velocity.x = velocity.x * (-1);
     }
+    private void defineItem(final FruitType type) {
+        switch (type) {
+        case MELON:
+            defineMelon();
+            break;
+        case ORANGE:
+            defineOrange();
+            break;
+        case CHERRY:
+            defineCherry();
+            break;
+        default:
+             break;
+        }
+     }
+     private void defineCherry() {
+         setRegion(screen.getAtlas().findRegion("NinjaAndEnemies"), 455, 9, 19, 16);
+         BodyDef cherryBody = new BodyDef();
+         cherryBody.position.set(getX(), getY());
+         cherryBody.type = BodyDef.BodyType.DynamicBody;
+         body = world.createBody(cherryBody);
+         FixtureDef cherryFixture = new FixtureDef();
+         CircleShape cherryShape = new CircleShape();
+         cherryShape.setRadius(FRUIT_RADIUS);
+         cherryFixture.shape = cherryShape;
+         maskBits(cherryFixture);
+         body.createFixture(cherryFixture).setUserData(this);
+     }
+     private void defineOrange() {
+         setRegion(screen.getAtlas().findRegion(REGION), 520, 9, 19, 16);
+         BodyDef orangeBody = new BodyDef();
+         orangeBody.position.set(getX(), getY());
+         orangeBody.type = BodyDef.BodyType.StaticBody;
+         body = world.createBody(orangeBody);
+         FixtureDef orangeFixture = new FixtureDef();
+         CircleShape orangeShape = new CircleShape();
+         orangeShape.setRadius(FRUIT_RADIUS);
+         orangeFixture.shape = orangeShape;
+         maskBits(orangeFixture);
+         body.createFixture(orangeFixture).setUserData(this);
+     }
+     private void defineMelon() {
+         setRegion(screen.getAtlas().findRegion(REGION), 486, 9, 19, 16);
+         BodyDef melonBody = new BodyDef();
+         melonBody.position.set(getX(), getY());
+         melonBody.type = BodyDef.BodyType.DynamicBody;
+         body = world.createBody(melonBody);
+         FixtureDef melonFixture = new FixtureDef();
+         CircleShape melonShape = new CircleShape();
+         melonShape.setRadius(FRUIT_RADIUS);
+         melonFixture.shape = melonShape;
+         maskBits(melonFixture);
+         body.createFixture(melonFixture).setUserData(this);
+     }
+     private void maskBits(final FixtureDef fruitFixture) {
+         fruitFixture.filter.categoryBits = GameConst.FRUIT;
+         fruitFixture.filter.maskBits = GameConst.NINJA_BIT
+                         | GameConst.GROUND_BIT
+                         | GameConst.GROUNDOBJECT_BIT
+                         | GameConst.BRICK_BIT
+                         | GameConst.COIN_BIT;
+     }
+     private void destroy() {
+        toDestroy = true;
+     }
 
 }
