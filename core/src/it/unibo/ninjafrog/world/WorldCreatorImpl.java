5d604301dedc7ae4607f22ed97b7be9bfd1177f3
Marsild <marsild.spahiu@studio.unibo.it>
2021-04-09 10:10:55 +0200

    create class WorldCreatorImpl

---
+++ core/src/it/unibo/ninjafrog/world/WorldCreatorImpl.java
@@ -0,0 +1,11 @@
+package it.unibo.ninjafrog.world;
+
+public class WorldCreatorImpl implements WorldCreator {
+
+    @Override
+    public void createWorld() {
+        // TODO Auto-generated method stub
+
+    }
+
+}
