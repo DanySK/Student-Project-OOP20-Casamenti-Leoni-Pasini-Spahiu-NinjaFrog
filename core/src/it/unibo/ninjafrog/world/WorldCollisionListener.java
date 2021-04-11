601a6dfe17209ce4d252bf245ed9b7570f78caf9
Marsild <marsild.spahiu@studio.unibo.it>
2021-04-11 14:30:33 +0200

    Create WorldContactListener

---
+++ core/src/it/unibo/ninjafrog/world/WorldCollisionListener.java
@@ -0,0 +1,34 @@
+package it.unibo.ninjafrog.world;
+
+import com.badlogic.gdx.physics.box2d.Contact;
+import com.badlogic.gdx.physics.box2d.ContactImpulse;
+import com.badlogic.gdx.physics.box2d.ContactListener;
+import com.badlogic.gdx.physics.box2d.Manifold;
+
+public class WorldCollisionListener implements ContactListener {
+
+    @Override
+    public void beginContact(final Contact contact) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void endContact(final Contact contact) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void preSolve(final Contact contact, final Manifold oldManifold) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void postSolve(final Contact contact, final ContactImpulse impulse) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
