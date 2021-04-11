4a2d7be7a2ccfbfa503f69c780ebdd9d5e74b663
Luca <luca.pasini8@studio.unibo.it>
2021-04-11 22:30:16 +0200

    create some implementation for the interfaces

--- core/src/it/unibo/ninjafrog/enemies/RinoModelImpl.java
+++ core/src/it/unibo/ninjafrog/enemies/RinoModelImpl.java
@@ -1,5 +1,29 @@
 package it.unibo.ninjafrog.enemies;
 
-public class RinoModel {
+public class RinoModelImpl implements RinoModel{
 
+    @Override
+    public void reverseVelocity(boolean b, boolean c) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public boolean isSetToDestroy() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public void defineEnemy() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void update(float dt) {
+        // TODO Auto-generated method stub
+        
+    }
+    
 }
--- core/src/it/unibo/ninjafrog/enemies/RinoViewImpl.java
+++ core/src/it/unibo/ninjafrog/enemies/RinoViewImpl.java
@@ -1,5 +1,38 @@
 package it.unibo.ninjafrog.enemies;
 
-public class RinoView {
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.physics.box2d.Body;
+
+public class RinoViewImpl implements RinoView {
+
+    @Override
+    public void update(Body b2body, float dt) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void setDeathRegion() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public float getY() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public float getX() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public void draw(Batch batch) {
+        // TODO Auto-generated method stub
+        
+    }
 
 }
