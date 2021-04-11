f9bad7bc84e87ea0436e7e960a7ee9731c1ff1d1
Marsild <marsild.spahiu@studio.unibo.it>
2021-04-11 14:37:34 +0200

    add javadoc to collision listener

--- core/src/it/unibo/ninjafrog/world/WorldCollisionListener.java
+++ core/src/it/unibo/ninjafrog/world/WorldCollisionListener.java
@@ -4,31 +4,27 @@ import com.badlogic.gdx.physics.box2d.Contact;
 import com.badlogic.gdx.physics.box2d.ContactImpulse;
 import com.badlogic.gdx.physics.box2d.ContactListener;
 import com.badlogic.gdx.physics.box2d.Manifold;
-
+/**
+ * Definition of the WorldCollisionListener class,
+ * it detects all collisions in the game and handles all the different cases.
+ * Implementation of the {@link com.badlogic.gdx.physics.box2d.ContactListener ContactListener} interface.
+ */
 public class WorldCollisionListener implements ContactListener {
 
     @Override
     public void beginContact(final Contact contact) {
-        // TODO Auto-generated method stub
-
     }
 
     @Override
     public void endContact(final Contact contact) {
-        // TODO Auto-generated method stub
-
     }
 
     @Override
     public void preSolve(final Contact contact, final Manifold oldManifold) {
-        // TODO Auto-generated method stub
-
     }
 
     @Override
     public void postSolve(final Contact contact, final ContactImpulse impulse) {
-        // TODO Auto-generated method stub
-
     }
 
 }
