8a793508f24da05f4af551731b303f3af53d5a4c
Gianmaria C <gianmaria.casamenti@studio.unibo.it>
2021-04-12 16:46:06 +0200

    create Menu interface

--- core/src/it/unibo/ninjafrog/screens/MainMenu.java
+++ core/src/it/unibo/ninjafrog/screens/MainMenu.java
@@ -1,52 +1,105 @@
 package it.unibo.ninjafrog.screens;
 
-import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+
+import it.unibo.ninjafrog.game.NinjaFrogGame;
+import it.unibo.ninjafrog.utilities.GameConst;
 /**
  * Definition of a MainMenu, which is an implementation of Screen.
  *
  */
-public class MainMenu implements Screen {
-
-    @Override
-    public void show() {
-        // TODO Auto-generated method stub
-        
+public class MainMenu implements Menu {
+    private NinjaFrogGame game;
+    private final Stage stage;
+    private final Viewport viewport;
+    private final Label playLabel;
+    private final Label settingsLabel;
+    private final Label quitLabel;
+    private int currentLabel;
+    private final Texture background;
+    private final Texture selector;
+    private Label.LabelStyle font;
+   /**
+    * 
+    * @param game
+    */
+    public MainMenu(final NinjaFrogGame game) {
+        this.game = game;
+        viewport = new FitViewport(GameConst.WIDTH, GameConst.HEIGHT, new OrthographicCamera());
+        viewport.apply();
+        stage = new Stage(viewport, game.getBatch());
+        Gdx.input.setInputProcessor(stage);
+        background = new Texture("Menu1background.png");
+        selector = new Texture("Selector.png");
+        font = new Label.LabelStyle(new BitmapFont(), Color.WHITE);
+        currentLabel = 1;
+        playLabel = new Label("Play", font);
+        settingsLabel = new Label("Setting", font);
+        quitLabel = new Label("Exit", font);
+        final Table table = new Table();
+        table.center();
+        table.setFillParent(true);
+        table.row();
+        table.add(playLabel);
+        table.row();
+        table.add(settingsLabel);
+        table.row();
+        table.add(quitLabel);
+        stage.addActor(table);
     }
-
-    @Override
-    public void render(float delta) {
-        // TODO Auto-generated method stub
-        
-    }
-
-    @Override
-    public void resize(int width, int height) {
-        // TODO Auto-generated method stub
-        
-    }
-
-    @Override
-    public void pause() {
-        // TODO Auto-generated method stub
-        
-    }
-
-    @Override
-    public void resume() {
-        // TODO Auto-generated method stub
-        
-    }
-
-    @Override
-    public void hide() {
-        // TODO Auto-generated method stub
-        
-    }
-
-    @Override
-    public void dispose() {
-        // TODO Auto-generated method stub
-        
-    }
-
+@Override
+public void show() {
+    // TODO Auto-generated method stub
+    
+}
+@Override
+public void render(float delta) {
+    // TODO Auto-generated method stub
+    
+}
+@Override
+public void resize(int width, int height) {
+    // TODO Auto-generated method stub
+    
+}
+@Override
+public void pause() {
+    // TODO Auto-generated method stub
+    
+}
+@Override
+public void resume() {
+    // TODO Auto-generated method stub
+    
+}
+@Override
+public void hide() {
+    // TODO Auto-generated method stub
+    
+}
+@Override
+public void dispose() {
+    // TODO Auto-generated method stub
+    
+}
+@Override
+public void handleInput() {
+    // TODO Auto-generated method stub
+    
+}
+@Override
+public void setMenu() {
+    // TODO Auto-generated method stub
+    
+}
+  
 }
---
+++ core/src/it/unibo/ninjafrog/screens/Menu.java
@@ -0,0 +1,14 @@
+package it.unibo.ninjafrog.screens;
+
+import com.badlogic.gdx.Screen;
+
+public interface Menu extends Screen {
+    /**
+     * 
+     */
+    void handleInput();
+    /**
+     * 
+     */
+    void setMenu();
+}
