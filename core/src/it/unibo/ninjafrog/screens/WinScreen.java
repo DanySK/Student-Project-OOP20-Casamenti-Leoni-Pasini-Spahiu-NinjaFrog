5f43abd06b8f41932a13244fb578c43d190b00f4
Gianmaria C <gianmaria.casamenti@studio.unibo.it>
2021-04-13 14:26:18 +0200

    create WinScreen classs

---
+++ core/src/it/unibo/ninjafrog/screens/WinScreen.java
@@ -0,0 +1,55 @@
+package it.unibo.ninjafrog.screens;
+
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+
+/**
+ * 
+ *
+ */
+public class WinScreen implements Screen{
+    private final Stage stage;
+    
+    @Override
+    public void show() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void render(float delta) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void pause() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void resume() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void hide() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void dispose() {
+        // TODO Auto-generated method stub
+        
+    }
+
+}
