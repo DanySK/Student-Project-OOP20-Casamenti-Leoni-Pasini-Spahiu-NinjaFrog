1d58385ff7887fe3f4e10259defd88b07c49bb9a
Marsild <marsild.spahiu@studio.unibo.it>
2021-04-14 15:06:20 +0200

    Create levelTwo class

---
+++ core/src/it/unibo/ninjafrog/screens/LevelTwo.java
@@ -0,0 +1,13 @@
+package it.unibo.ninjafrog.screens;
+/**
+ * LevelTwo class definition. Second level of the game.
+ * Implementation of {@link it.unibo.ninjafrog.screens.Level Level} interface.
+ */
+public class LevelTwo implements Level {
+
+    @Override
+    public final String getMap() {
+        return "level2.tmx";
+    }
+
+}
