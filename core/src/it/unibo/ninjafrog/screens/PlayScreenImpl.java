574e9eff186df36895113285c71d1abc978e2368
Marsild <marsild.spahiu@studio.unibo.it>
2021-04-14 16:22:48 +0200

    create cam in playscreen

--- core/src/it/unibo/ninjafrog/screens/PlayScreenImpl.java
+++ core/src/it/unibo/ninjafrog/screens/PlayScreenImpl.java
@@ -1,24 +1,32 @@
 package it.unibo.ninjafrog.screens;
 
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.physics.box2d.World;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 
 import it.unibo.ninjafrog.fruits.FruitType;
 import it.unibo.ninjafrog.game.NinjaFrogGame;
 import it.unibo.ninjafrog.hud.Hud;
 import it.unibo.ninjafrog.screens.levels.Level;
+import it.unibo.ninjafrog.utilities.GameConst;
 import it.unibo.ninjafrog.utilities.Pair;
 import it.unibo.ninjafrog.world.Collidable;
 /**
  * Implementation of the {@link it.unibo.ninjafrog.screens.PlayScreen PlayScreen}.
  */
 public final class PlayScreenImpl implements PlayScreen {
+    private static final int CAM_Z_COMPONENT = 0;
+    private static final int HALF = 2;
     private final TextureAtlas atlas;
     private final NinjaFrogGame game;
     private Hud hud;
     private final TiledMap map;
+    private final OrthographicCamera cam;
+    private final Viewport viewport;
     /**
      * Public constructor of the PlayScreenImpl.
      * @param game The {@link it.unibo.ninjafrog.game.NinjaFrogGame game} class.
@@ -28,6 +36,21 @@ public final class PlayScreenImpl implements PlayScreen {
         this.game = game;
         this.atlas = new TextureAtlas("ninjaAndEnemies.pack");
         this.map = new TmxMapLoader().load(level.getMap());
+        this.cam = new OrthographicCamera();
+        this.viewport = new FitViewport(scale(GameConst.WIDTH),
+                scale(GameConst.HEIGHT),
+                this.cam);
+        this.cam.position.set(halfOf(this.viewport.getWorldWidth()),
+                halfOf(this.viewport.getWorldHeight()),
+                CAM_Z_COMPONENT);
+    }
+
+    private float halfOf(final float value) {
+        return value / HALF;
+    }
+
+    private float scale(final int value) {
+        return value / GameConst.PPM;
     }
 
     @Override
