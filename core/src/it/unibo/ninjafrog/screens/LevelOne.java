752437de7e738e0c55a13030f7dbf6dc04e3def3
Marsild <marsild.spahiu@studio.unibo.it>
2021-04-14 15:04:04 +0200

    create level one

---
+++ core/src/it/unibo/ninjafrog/screens/LevelOne.java
@@ -0,0 +1,13 @@
+package it.unibo.ninjafrog.screens;
+/**
+ * LevelOne class definition.
+ * Implementation of {@link it.unibo.ninjafrog.screens.Level Level} interface.
+ */
+public final class LevelOne implements Level {
+
+    @Override
+    public String getMap() {
+        return "level1.tmx";
+    }
+
+}
