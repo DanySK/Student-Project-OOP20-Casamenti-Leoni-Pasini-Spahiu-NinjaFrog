2f53915bc486bf2ba36af8ab50d59db26f0ee2ed
Gianmaria C <gianmaria.casamenti@studio.unibo.it>
2021-04-19 16:37:00 +0200

    add MenuTest with javaDoc

---
+++ tests/src/it/unibo/ninjafrog/tests/MenuTests.java
@@ -0,0 +1,42 @@
+package it.unibo.ninjafrog.tests;
+
+import org.junit.runner.RunWith;
+
+/**
+ * Automated tests for all menu in the game.
+ * MenuTests includes WinScreen and GameOverScreen.
+ */
+@RunWith(GdxTestRunner.class)
+public class MenuTests {
+	
+	/**
+	 * Test for the MainMenu background file.
+	 */
+	public void MainMenuExists() {
+		
+	}
+	/**
+	 * Test for the SettingsMenu background file.
+	 */
+	public void SettingsMenuExists() {
+		
+	}
+	/**
+	 * Test for the LevelMenu background file.
+	 */
+	public void LevelsMenu() {
+		
+	}
+	/**
+	 * Tests for the WinScreen background file.
+	 */
+	public void WinScreenExists() {
+		
+	}
+	/**
+	 * Tests for the GameOverScreen background file.
+	 */
+	public void GameOverExists() {
+		
+	}
+}
