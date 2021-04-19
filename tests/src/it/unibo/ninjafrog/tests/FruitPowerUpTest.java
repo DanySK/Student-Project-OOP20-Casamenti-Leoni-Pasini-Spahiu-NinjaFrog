d18ca69afde5265a642f72ecd38d82f3f928dd76
Gianmaria C <gianmaria.casamenti@studio.unibo.it>
2021-04-19 17:15:46 +0200

    implements FruitPowerUptests

--- tests/src/it/unibo/ninjafrog/tests/FruitPowerUpTest.java
+++ tests/src/it/unibo/ninjafrog/tests/FruitPowerUpTest.java
@@ -1,5 +1,34 @@
 package it.unibo.ninjafrog.tests;
 
-public class FruitPowerUpTest {
+import org.junit.runner.RunWith;
 
+/**
+ * Automated tests for the FruitPowerUp: cherry, melon, orange. 
+ */
+@RunWith(GdxTestRunner.class)
+public class FruitPowerUpTest {
+	/**
+	 * Test for the Melon power up.
+	 */
+	public void MelonExists() {
+		
+	}
+	/**
+	 * Test for the Cherry power up.
+	 */
+	public void CherryExists() {
+		
+	}
+	/**
+	 * Test for the Orange power up.
+	 */
+	public void OrangeExists() {
+		
+	}
+	/**
+	 * Test for the asset file
+	 */
+	public void assetExists() {
+		
+	}
 }
